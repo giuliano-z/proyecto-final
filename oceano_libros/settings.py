a111f7ed77baaec39e205b6627ad5feeef3aec3b
Giuliano-z <giulianodanielzulatto@gmail.com>
2023-07-14 11:45:57 -0300

    templates basicos

--- oceano_libros/settings.py
+++ oceano_libros/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'oceano_libros.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
